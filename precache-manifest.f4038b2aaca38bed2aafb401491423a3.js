fa68e2f31ed3d235c23102c2ca627818a3ad01b4
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2020-09-27 12:42:03 +0300

    Updates

---
+++ precache-manifest.f4038b2aaca38bed2aafb401491423a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adda64625adb94343536829b24b8f69",
+    "url": "/magaz/index.html"
+  },
+  {
+    "revision": "eeb51cd786eec75c9784",
+    "url": "/magaz/static/css/main.2daf376a.chunk.css"
+  },
+  {
+    "revision": "ab4579c1c5f40ef2e91f",
+    "url": "/magaz/static/js/2.bb8503b9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/magaz/static/js/2.bb8503b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb51cd786eec75c9784",
+    "url": "/magaz/static/js/main.b812c630.chunk.js"
+  },
+  {
+    "revision": "4da24308290d0895634d",
+    "url": "/magaz/static/js/runtime-main.adc09d54.js"
+  }
+]);
